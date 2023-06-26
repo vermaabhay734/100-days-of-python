52126dbb0aca922d6a605bb3f8eef610d2d778d4
abhay734 <vermaabhay734@gmail.com>
2023-06-26 13:34:55 +0530

    Data type and BMI Calculator

---
+++ Day 2/Data_types.py
@@ -0,0 +1,11 @@
+two_digit_number = input("Type a two digit number: ")
+
+a = two_digit_number[0]
+b = two_digit_number[1]
+
+print("Sum of two digit numbers are: ")
+print(int(a)+int(b))
+
+# Type a two digit number: 25
+# Sum of two digit numbers are: 
+# 7
---
+++ Day 2/Math_Operation.py
@@ -0,0 +1,17 @@
+3 + 5  
+7 - 4
+3 * 2   
+6 / 3
+2 ** 3
+
+
+# Priority : PEMDAS
+# Parenthesis : ()
+# exponent : **
+# Multiplication : *
+# Division : /
+# Addition : +
+# Subtraction : -
+
+print(3 * 3 + 3 / 3 - 3)  #7
+print(3 * (3 + 3) / 3 - 3)  #3

3ed52e78e92af66123f10e42cc14e6f5f650d277
abhay734 <vermaabhay734@gmail.com>
2023-07-11 19:14:28 +0530

    List and list functions

---
+++ ListOfStates.py
@@ -0,0 +1,11 @@
+print("This is the list of all state present in the America.")
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+states_of_america[1] = "Pencilvania" #It changes the list
+
+#states_of_america.append()
+
+print(states_of_america[5]) #For printing 6th element. 
+# print(states_of_america) for all the list.    
+

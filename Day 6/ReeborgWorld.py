0fa566f16552055641004b03e010d056587c046d
abhay734 <vermaabhay734@gmail.com>
2023-07-27 12:08:39 +0530

    Reeborg's World : Around 1 - Apple

---
+++ Day 6/ReeborgWorld.py
@@ -0,0 +1,18 @@
+# Around 1 - Apple in Reeborg's World
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Around%201%20-%20apple&url=worlds%2Ftutorial_en%2Faround1c.json
+
+# Go to the link run the program.
+
+'''
+def line():
+    for i in range(9):
+        move()
+        if object_here():
+            take()
+def square():
+    line()
+    turn_left()
+    
+for i in range(4):
+    square()
+'''

c7c4e4b2b5e2f9c7da986b5c3fd166558c5abd17
abhay734 <vermaabhay734@gmail.com>
2023-07-31 23:02:53 +0530

    Function with positional Arguments

---
+++ Day 8/function_with_args.py
@@ -0,0 +1,8 @@
+# function with positional Arguments.
+def greet_with_input(name, city):
+    print(f"Hello {name}")
+    print(f"Good Morning {name}.")
+    print(f"You are from {city}.")
+
+greet_with_input(name = "Abhay",city = "Agra")
+# greet_with_input(city = "Agra",name = "Abhay")
---
+++ Day 8/function_with_input.py
@@ -0,0 +1,17 @@
+# function
+def greet():
+    print("Hello name")
+    print("Good Morning Name")
+    print("You are from city.")
+
+# greet()
+
+# function with input
+def greet_with_input(name, city):
+    print(f"Hello {name}")
+    print(f"Good Morning {name}.")
+    print(f"You are from {city}.")
+
+greet_with_input("Abhay", "Agra")
+
+

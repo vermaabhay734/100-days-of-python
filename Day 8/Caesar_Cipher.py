59dbf1baa1128f8aa332e54c45a3911fab2ee9ef
abhay734 <vermaabhay734@gmail.com>
2023-08-01 17:52:04 +0530

    Day 8 Caesar Cipher Project

--- Day 8/Caesar_Cipher.py
+++ Day 8/Caesar_Cipher.py
@@ -1,29 +1,31 @@
+import Carsar_Cipher_logo
+print(Carsar_Cipher_logo.logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(direction, text, shift):
+    end_text = ""
+    if direction == "decode":
+        shift *= -1
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {direction}d text is {end_text}")
 
-def encrypt(text, shift):
-    cipher_text = ""
-    for letter in text:
-        position = alphabet.index(letter)
-        new_position = position + shift
-        cipher_text += alphabet[new_position]
-    print(f"The encoded text is {cipher_text}")
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    # If the user enters a shift that is greater than the number of the letter in the alphabet
+    shift = shift % 26
+    caesar(direction, text, shift)
 
-def decrypt(cipher_text, shift):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift
-        plain_text += alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
-
-
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
-else:
-    print("Please write correct word!")
+    result = input('''Type "yes" if you want to go again, Otherwise "no"''')
+    if result == "no":
+        should_continue = False
+        print("Good Bye!")
